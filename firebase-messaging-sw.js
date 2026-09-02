0bdab2dbaae4c5905462d840de73294e3fbc72bd
Meyerfoo20 <158509755+Meyerfoo20@users.noreply.github.com>
2026-09-02 14:20:32 +0200

    Add files via upload

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -24,7 +24,7 @@ messaging.onBackgroundMessage((payload) => {
   self.registration.showNotification(notificationTitle, notificationOptions);
 });
 
-const CACHE_NAME = 'te4-chatt-v38';
+const CACHE_NAME = 'te4-chatt-v39';
 const ASSETS_TO_CACHE = [
   './',
   './index.html',
